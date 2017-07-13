10ff78fb215f6748a57ef29f1ea8b8c5efbb0024
Sourabh Verma <sourabh.coder@gmail.com>
2017-07-13 17:08:28 +0530

    Migrate to RxJS 5 and ES6 syntax

--- index.js
+++ index.js
@@ -1,57 +1,57 @@
-var Rx = require('rx');
-var Contacts = require('react-native-contacts');
+import { Observable } from 'rxjs/Rx'
+import Contacts from 'react-native-contacts'
 
 function _execAddressBookMethodWithParamsErrorData(func) {
-  return Rx.Observable.create(function(observer) {
-    func(function(error, data) {
-      if (error) {
-        observer.onError(error);
-      } else {
-        observer.onNext(data);
-        observer.onCompleted();
-      }
-    });
+    return Observable.create(function (observer) {
+        func(function (error, data) {
+            if (error) {
+                observer.error(error)
+            } else {
+                observer.next(data)
+                observer.complete()
+            }
+        })
 
-    return null;
-  });
+        return null
+    })
 }
 
 function _execAddressBookMethodWithParamError(contact, func) {
-  return Rx.Observable.create(function(observer) {
-    func(contact, function(error) {
-      if (error) {
-        observer.onError(error);
-      } else {
-        observer.onCompleted();
-      }
-    });
+    return Observable.create(function (observer) {
+        func(contact, function (error) {
+            if (error) {
+                observer.error(error)
+            } else {
+                observer.complete()
+            }
+        })
 
-    return null;
-  });
+        return null
+    })
 }
 
-Contacts.rx_checkPermission = function() {
-  return _execAddressBookMethodWithParamsErrorData(Contacts.checkPermission);
-};
-
-Contacts.rx_requestPermission = function() {
-  return _execAddressBookMethodWithParamsErrorData(Contacts.requestPermission);
-};
-
-Contacts.rx_getAll = function() {
-  return _execAddressBookMethodWithParamsErrorData(Contacts.getAll);
-};
-
-Contacts.rx_addContact = function(contact) {
-  return _execAddressBookMethodWithParamError(contact, Contacts.addContact);
+Contacts.rx_checkPermission = function () {
+    return _execAddressBookMethodWithParamsErrorData(Contacts.checkPermission)
 }
 
-Contacts.rx_updateContact = function(contact) {
-  return _execAddressBookMethodWithParamError(contact, Contacts.updateContact);
+Contacts.rx_requestPermission = function () {
+    return _execAddressBookMethodWithParamsErrorData(Contacts.requestPermission)
 }
 
-Contacts.rx_deleteContact = function(contact) {
-  return _execAddressBookMethodWithParamError(contact, Contacts.deleteContact);
+Contacts.rx_getAll = function () {
+    return _execAddressBookMethodWithParamsErrorData(Contacts.getAll)
 }
 
-module.exports = Contacts;
+Contacts.rx_addContact = function (contact) {
+    return _execAddressBookMethodWithParamError(contact, Contacts.addContact)
+}
+
+Contacts.rx_updateContact = function (contact) {
+    return _execAddressBookMethodWithParamError(contact, Contacts.updateContact)
+}
+
+Contacts.rx_deleteContact = function (contact) {
+    return _execAddressBookMethodWithParamError(contact, Contacts.deleteContact)
+}
+
+export default Contacts
